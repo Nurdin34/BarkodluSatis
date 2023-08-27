9f2a5bd82df5087cbe406a5a95694991651dacab
Nurdin34 <tezcanurdin38@icloud.com>
2023-08-27 21:22:18 +0300

    Proje dosyası ekle.

---
+++ BarkodluSatis/fSatis.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarkodluSatis
+{
+    public partial class fSatis : Form
+    {
+        public fSatis()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer2_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button34_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button37_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button38_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button39_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button35_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button50_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button47_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button48_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button51_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button53_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button49_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button46_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button52_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        BarkodDbEntities db = new BarkodDbEntities();
+        private void tBarkod_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                string barkod =tBarkod.Text.Trim();
+                if(barkod.Length <= 2)
+                {
+                    tMiktar.Text = barkod;
+                    tBarkod.Clear();
+                    tBarkod.Focus();
+                }
+                else
+                {
+                    if (db.Urun.Any(a => a.Barkod == barkod))
+                    {
+                        var urun=db.Urun.Where(a=>a.Barkod==barkod).FirstOrDefault();
+                        int satirsayisi = gridSatisListesi.Rows.Count;
+                        double miktar = Convert.ToDouble(tMiktar.Text);
+                        bool eklenmismi = false;
+                        if(satirsayisi> 0)
+                        {
+                            for(int i =0;i<satirsayisi;i++)
+                            {
+                                if (gridSatisListesi.Rows[i].Cells["Barkod"].Value.ToString()==barkod)
+                                {
+                                    gridSatisListesi.Rows[i].Cells["Miktar"].Value = miktar + Convert.ToDouble(gridSatisListesi.Rows[i].Cells["Miktar"].Value);
+                                    gridSatisListesi.Rows[i].Cells["Toplam"].Value = Math.Round(Convert.ToDouble(gridSatisListesi.Rows[i].Cells["Miktar"].Value) * Convert.ToDouble(gridSatisListesi.Rows[i].Cells["Fiyat"].Value),2);
+                                    eklenmismi = true;
+                                }
+                            }
+                        }
+                        if (!eklenmismi)
+                        {
+                            gridSatisListesi.Rows.Add();
+                            gridSatisListesi.Rows[satirsayisi].Cells["Barkod"].Value = barkod;
+                            gridSatisListesi.Rows[satirsayisi].Cells["UrunAdi"].Value = urun.UrunAd;
+                            gridSatisListesi.Rows[satirsayisi].Cells["UrunGrup"].Value = urun.UrunGrup;
+                            gridSatisListesi.Rows[satirsayisi].Cells["Birim"].Value = urun.Birim;
+                            gridSatisListesi.Rows[satirsayisi].Cells["Fiyat"].Value = urun.SatisFiyat;
+                            gridSatisListesi.Rows[satirsayisi].Cells["Miktar"].Value = miktar;
+                            gridSatisListesi.Rows[satirsayisi].Cells["Toplam"].Value = Math.Round(miktar * (double)urun.SatisFiyat, 2);
+                            gridSatisListesi.Rows[satirsayisi].Cells["AlisFiyat"].Value = urun.AlisFiyat;
+                            gridSatisListesi.Rows[satirsayisi].Cells["KdvTutari"].Value = urun.KdvTutari;
+
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
